c03e3d0eec1183a0b5920952f98609dc109c353c
Ajay Singh <ajay.singh02@nagarro.com>
2015-04-27 11:14:25 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uhi::Application.config.secret_token = 'a7870a21ca38aa7f89fefdef8b089d4e0366d5a7eabc05f5733f32e5a63079c6b7b832dc1d5e5aeaf115e0e8a41e18cf1885c843bec4fb8f71c17126cac34a3b'
